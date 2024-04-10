6c7af32a2f674ce607e2b285c81778bff5a8f676
Tom Aldridge <tomaldridge12@icloud.com>
2024-04-10 14:28:35 +0100

    minor formatting updates

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ from utils import GameEvent, ThreadSafeQueue, TweepyClient
 def hourly_update_players(players: List[Player], in_match_players: deque[Player]) -> None:
     # repeat this every hour
     while not stop_event.is_set():
-        logging.info("Beginning hourly player check")
+        logging.info(f"Beginning hourly player check at {datetime.now()}")
+        # Iterate through all players in the player list
         for player in players:
             match_id = fm.get_next_match_id(player)
             try:
@@ -25,20 +26,23 @@ def hourly_update_players(players: List[Player], in_match_players: deque[Player]
                 traceback.print_exc()
                 logging.info(traceback.format_exc())
                 continue
+
             if isinstance(player.match_info, dict): # i.e. player is in lineup
                 if not player.in_queue:
                     if player.is_match_soon(player.match_info["match_details"]):
+                        # TODO: why am I using a queue system here instead of a set?
                         in_match_players.put(player)
                         player.in_queue = True
                         logging.info(f"Adding {player.name} to queue")
-        logging.info("Ending hourly player check")
-
+        
+        logging.info(f"Ending hourly player check at {datetime.now()}")
         sleep(600)
 
 def minutely_update_events(in_match_players: deque[Player]) -> None:
     # repeat this every couple of minutes
     while not stop_event.is_set():
 
+        # Iterate through all players in the in_match_players queue. Why not use a set here?
         for player in in_match_players:
             logging.info(f"Polling {player.name} at {datetime.now()}")
             if isinstance(player.match_info, dict): # otherwise error message from get_player_details_from_match
@@ -82,16 +86,19 @@ def minutely_update_events(in_match_players: deque[Player]) -> None:
         sleep(60)
 
 if __name__ == "__main__":
+    # Instantiate clients
     fm = FotMob()
     tc = TweepyClient()
     in_match_players = ThreadSafeQueue()
 
+    # Set up logging
     if not os.path.isdir("logs"):
         os.makedirs("logs")
     logfile_name = f"logs/{datetime.now().strftime("%d-%m-%Y-%H-%M-%S")}.log"
     logging.basicConfig(filename=logfile_name, level=logging.INFO)
     logging.info(f"Starting loanbot at {datetime.now()}")
 
+    # Load player list into Player array
     with open('ids.json', 'r') as f:
         player_data = json.load(f)
     
@@ -99,19 +106,17 @@ if __name__ == "__main__":
     players = [Player(name, data['id'], data['team_id'], data['team_name'])
                 for name, data in player_data.items()]
     
+    # Start API threads
     logging.info("Starting threads...")
     hourly_update = threading.Thread(target=hourly_update_players, args=(players,in_match_players,))
     events_update = threading.Thread(target=minutely_update_events, args=(in_match_players,))
 
     stop_event = threading.Event()
 
-    # hourly_update.daemon = True
-    # events_update.daemon = True
-
     hourly_update.start()
     events_update.start()
     
-    # Keep main thread alive
+    # Keep main thread alive and prepare for exiting
     try:
         while True:
             sleep(1)
